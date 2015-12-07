23b90223f732748ba04faf85cbfd9f222e25df47
Javier Cejudo <javier@javiercejudo.com>
2015-12-07 22:57:54 +1100

    feat: allow falsy proto names and use unsupported package

--- test/spec.js
+++ test/spec.js
@@ -13,11 +13,13 @@ describe('binary operation', function() {
     binaryOpExtender(Decimal, 'plus');
     binaryOpExtender(Decimal, 'plus', '+');
     binaryOpExtender(Decimal, 'div');
+    binaryOpExtender(Decimal, 'div', 0);
 
     new Decimal('2').plus(new Decimal('3')).toString().should.be.exactly('5');
     new Decimal('2')['+'](new Decimal('3')).toString().should.be.exactly('5');
 
-    new Decimal('1').div(new Decimal('2')).toString().should.be.exactly('0.5');
+    // allow falsy protoName's
+    new Decimal('1')[0](new Decimal('2')).toString().should.be.exactly('0.5');
   });
 
   it('should throw when the given method does not exist', function() {
